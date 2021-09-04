5f49896a2698148d598d4dd6e05348ae4ad6fc38
Bruno SC <bruno.sc.2006@gmail.com>
2021-09-04 14:55:05 -0300

    Update

---
+++ 1-Lista_de_Exercicios/CircleArea/AreaDoCirculo.java
@@ -0,0 +1,30 @@
+package circleArea;
+
+import java.util.Scanner;
+import java.lang.Math;
+
+//Objetivo:
+//Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro casas decimais.
+
+public class AreaDoCirculo {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		//Definindo valor de pi
+		Double pi = 3.14159;
+		
+		//Perguntando e lendo o raio
+		System.out.print("Insira o raio do círculo: ");
+		Double raio = scan.nextDouble();
+		
+		//Realizado o calculo (area = π . raio²)
+		Double area = pi * (Math.pow(raio, 2));
+		
+		//Mostrando o resultado
+		System.out.printf("A area do circulo é: %.4f", area);
+		
+		scan.close();
+	}
+
+}
---
+++ 1-Lista_de_Exercicios/SumIntNumers/SumIntNumbers.java
@@ -0,0 +1,28 @@
+package sumIntNumbers;
+
+import java.util.Scanner;
+
+//Objetivo:
+//Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma mensagem explicativa.
+
+public class SumIntNumbers {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		//Perguntando e lendo os inteiros
+		System.out.print("Insira o primeiro valor: ");
+		int val1 = scan.nextInt();
+		System.out.print("Insira o segundo valor: ");
+		int val2 = scan.nextInt();
+		
+		//Realizando a somatória
+		int soma = val1 + val2;
+		
+		//Devolvendo o resultado da operação
+		System.out.printf("A soma de %d + %d = %d",val1,val2,soma);
+		
+		scan.close();
+	}
+
+}
