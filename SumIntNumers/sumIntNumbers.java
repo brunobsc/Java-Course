8d5a9389305e66b657aed8bc8d27b4599ec18873
Bruno SC <bruno.sc.2006@gmail.com>
2021-09-04 14:38:03 -0300

    Exercicio 2

--- SumIntNumers/sumIntNumbers.java
+++ SumIntNumers/sumIntNumbers.java
@@ -2,6 +2,9 @@ package sumIntNumbers;
 
 import java.util.Scanner;
 
+//Objetivo:
+//Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma mensagem explicativa.
+
 public class SumIntNumbers {
 
 	public static void main(String[] args) {
