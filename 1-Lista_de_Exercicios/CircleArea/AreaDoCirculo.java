0efcd0e3baaf535022eee1877dc1916c35d260f0
Bruno SC <bruno.sc.2006@gmail.com>
2021-09-04 15:36:07 -0300

    Update

--- 1-Lista_de_Exercicios/CircleArea/AreaDoCirculo.java
+++ 1-Lista_de_Exercicios/CircleArea/AreaDoCirculo.java
@@ -11,15 +11,18 @@ public class AreaDoCirculo {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
+		//Declarando variaveis que serão usadas
+		Double pi,raio,area;
+		
 		//Definindo valor de pi
-		Double pi = 3.14159;
+		pi = 3.14159;
 		
 		//Perguntando e lendo o raio
 		System.out.print("Insira o raio do círculo: ");
-		Double raio = scan.nextDouble();
+		raio = scan.nextDouble();
 		
 		//Realizado o calculo (area = π . raio²)
-		Double area = pi * (Math.pow(raio, 2));
+		area = pi * (Math.pow(raio, 2));
 		
 		//Mostrando o resultado
 		System.out.printf("A area do circulo é: %.4f", area);
